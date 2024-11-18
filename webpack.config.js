935ca87a7a2bdfdb607c1e73780fe4a05d6d5cf3
Natasha01013 <natasha01013@yandex.ru>
2024-11-18 17:28:02 +0100

    change serve

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
+
+
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
@@ -11,34 +13,41 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
-      publicPath: 'path.dist', //  publicPath: params.path, // Префикс пути, по которому будут лежать файлы у вебпак дев сервера 
-      // Например: '/public/assets/' - файлы будут доступны по адресу: http://localhost/public/assets/file_name.js
+      directory: path.join(__dirname, 'public'),
     },
-    host: 'localhost', // Host webpack dev server'a
-    port: 8080, // Port webpack dev server'a
-    hot: true, // Включить автоперезагрузку страницы при изменении файлов
-    historyApiFallback: { index: "index.html" },
-    proxy: [
-    { // Собственно настройки прокси
-   //   '**': { // С какого адреса webpack dev server'a будут проксироваться запросы на адрес локального сервера. 
-        // В данном случае ** - значит все адреса проксировать, начиная с корня http://localhost:8080
-        // Например: http://localhost:8080/user запрос на http://localhost/user (адрес из target ниже)
-        context: ['/api'],
-        target: `http://localhost:8080`, // Целевой адрес локального сервера, куда будут проксироваться запросы с дев сервера 
-        onProxyRes(proxyRes, req, res) {
-          if (proxyRes.headers.location) { // Если есть заголовок со свойством location (Где храниться полный адрес запроса к локальному серверу)
-            let location = proxyRes.headers.location; // Сохраняем адрес зоголовка location из ответа в переменную location
-            console.log(`LOCATION: ${proxyRes.headers.location}`); // Выводит в консоль адрес до замены
-            location = location.replace('localhost', 'localhost:8080'); // Заменяем адрес локального сервера на адрес webpack dev server'a
-            proxyRes.headers.location = location; // Присваиваем в заголовок location подмененный адрес из переменной location
-            console.log(`REPLACED: ${proxyRes.headers.location}`); // Выводит в консоль адрес после замены
-          }
-//        }
-      }
-    }
-  ]
+    compress: true,
+    port: 8080,
   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//       publicPath: 'path.dist', //  publicPath: params.path, // Префикс пути, по которому будут лежать файлы у вебпак дев сервера 
+//       // Например: '/public/assets/' - файлы будут доступны по адресу: http://localhost/public/assets/file_name.js
+//     },
+//     host: 'localhost', // Host webpack dev server'a
+//     port: 8080, // Port webpack dev server'a
+//     hot: true, // Включить автоперезагрузку страницы при изменении файлов
+//     historyApiFallback: { index: "index.html" },
+//     proxy: [
+//     { // Собственно настройки прокси
+//    //   '**': { // С какого адреса webpack dev server'a будут проксироваться запросы на адрес локального сервера. 
+//         // В данном случае ** - значит все адреса проксировать, начиная с корня http://localhost:8080
+//         // Например: http://localhost:8080/user запрос на http://localhost/user (адрес из target ниже)
+//         context: ['/api'],
+//         target: `http://localhost:8080`, // Целевой адрес локального сервера, куда будут проксироваться запросы с дев сервера 
+//         onProxyRes(proxyRes, req, res) {
+//           if (proxyRes.headers.location) { // Если есть заголовок со свойством location (Где храниться полный адрес запроса к локальному серверу)
+//             let location = proxyRes.headers.location; // Сохраняем адрес зоголовка location из ответа в переменную location
+//             console.log(`LOCATION: ${proxyRes.headers.location}`); // Выводит в консоль адрес до замены
+//             location = location.replace('localhost', 'localhost:8080'); // Заменяем адрес локального сервера на адрес webpack dev server'a
+//             proxyRes.headers.location = location; // Присваиваем в заголовок location подмененный адрес из переменной location
+//             console.log(`REPLACED: ${proxyRes.headers.location}`); // Выводит в консоль адрес после замены
+//           }
+// //        }
+//       }
+//     }
+//   ]
+//   },
   module: {
     rules: [
       {
